72ee2dff448f668830e8b19607d45a703bc3488c
hussamh10 <hussamh10@gmail.com>
2018-01-10 20:03:09 +0500

    v1.1

---
+++ libs/utils.py
@@ -0,0 +1,38 @@
+from libs.folder import Folder
+import os
+import re
+
+def generateBasicRegex(q):
+    basic_ptrn = '.*'.join(q)
+    return re.compile(basic_ptrn)
+
+def generateSubRegex(q):
+    return re.compile(q)
+
+def readFile(file_name):
+    return open(file_name, 'r').readlines()
+
+def initFoldersList(file_name):
+    file = readFile(file_name)
+    folders = []
+    for path in file:
+        folders.append(Folder(path))
+    return folders
+
+def sort(f):
+    return sorted(f, key=lambda x: x.score, reverse=True)
+
+def foldersString(f):
+    temp = f[0:3]
+    string = ''
+    for t in temp:
+        string = string + (t.path) + '\n'
+    return string
+
+def printFolders(f):
+    temp = f[0:3]
+    for t in temp:
+        print(t.path[:-1])
+
+def openFolder(folder):
+    os.system('explorer \"' + folder.path + '\"')
---
+++ main.py
@@ -0,0 +1,42 @@
+import os
+from msvcrt import getch
+from libs.index import index
+import libs.scores as scores
+import libs.utils as utils
+
+def main():
+    rootDir = os.environ["HOMEPATH"]
+    indexFilePath = os.path.join(rootDir, 'indexed')
+
+    if not os.path.isfile(indexFilePath):
+        print("Indexing, this only happens the first time, kindly repot back the number below (when it appears")
+        index()
+
+    folders = utils.initFoldersList(indexFilePath)
+
+    exit = False
+    q = ''
+    result = 'None'
+
+    while not exit:
+        char = chr(ord(getch()))
+        q = q + char
+        basic_rx = utils.generateBasicRegex(q)
+        sub_rx = utils.generateSubRegex(q)
+
+        if char == '\r':
+            if len(folders) == 0:
+                print("Folder not found")
+            else:
+                folder = folders[0]
+                utils.openFolder(folder)
+            exit = True
+
+        folders = scores.updateScores(basic_rx, sub_rx, folders)
+        folders = scores.advancedScoreUpdate(q, folders)
+        folders = utils.sort(folders)
+
+        utils.printFolders(folders)
+        result = utils.foldersString(folders)
+
+main()
