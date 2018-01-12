6f73a03b411abaefe09dbd16aadb15e4d156823f
hussamh10 <hussamh10@gmail.com>
2018-01-12 17:27:02 +0500

    added space support

--- src/libs/scores.py
+++ src/libs/scores.py
@@ -3,7 +3,7 @@ import re
 def updateScores(basic_rx, sub_rx, folders):
     temp = []
     for folder in folders:
-        folder.score = getScore(basic_rx, sub_rx, folder.name.lower())
+        folder.score += getScore(basic_rx, sub_rx, folder.name.lower())
 
         if folder.score > 0:
             temp.append(folder)
@@ -26,6 +26,24 @@ def advancedScoreUpdate(q, folders):
 
     return folders
 
+def SpaceScores(q, r, folders):
+    if ' ' not in q:
+        return folders
+    
+    temp = []
+    for folder in folders:
+        string = folder.path.split('\\')[:-1]
+        string = ' '.join(string)
+
+        match = r.search(string.lower())
+
+        if not match == None:
+            folder.score += 10
+            temp.append(folder)
+
+    return temp
+
+
 def getScore(basic_rx, sub_rx, string):
     match_basic = basic_rx.search(string)
     match_sub = sub_rx.search(string)
@@ -36,6 +54,7 @@ def getScore(basic_rx, sub_rx, string):
     score = 1
     
     if match_basic.start() == 0:
+
         score += 10
 
     if match_basic.end() == len(match_basic.string):
@@ -44,6 +63,6 @@ def getScore(basic_rx, sub_rx, string):
     match_substring = sub_rx.search(string)
 
     if not match_substring == None:
-        score += 20
+        score += 10
 
     return score
